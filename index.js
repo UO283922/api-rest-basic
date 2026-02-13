f7dde561262a0fc642f48eadacfad722b632989d
uo283922@uniovi.es <uo283922@uniovi.es>
2026-02-13 16:37:20 +0100

    servicio REST /api/datos y CORS

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./app/app');
+
+// Listening port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Service listening at port ${port}`);
+});
+
